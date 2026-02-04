f500d9889fd47064be4c80320e33dad0f53e0b8b
ivanhuertasg <ivanhuertas05@gmail.com>
2026-02-04 12:41:44 +0100

    Export components from index.ts instead of hook
    Replace the previous hook export (useCart) with component exports from the package entrypoint: LanguageSelector, MiniCart, and ProductCard. Also remove the obsolete duplicate file index(1).ts. This centralizes component exports in the root index.

--- index(1).ts
+++
@@ -1,4 +0,0 @@
-// Components exports
-export { default as LanguageSelector } from './LanguageSelector';
-export { default as MiniCart } from './MiniCart';
-export { default as ProductCard } from './ProductCard';
--- index.ts
+++ index.ts
@@ -1,2 +1,4 @@
-// Hooks exports
-export { useCart } from './useCart';
+// Components exports
+export { default as LanguageSelector } from './LanguageSelector';
+export { default as MiniCart } from './MiniCart';
+export { default as ProductCard } from './ProductCard';
